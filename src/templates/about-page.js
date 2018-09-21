d9b7d65f1e37ec6f55ed7b8bd005f9aef1be74c9
Fuxa, Jan <jan.fuxa@firma.seznam.cz>
2018-09-21 14:46:47 +0200

    workflow editorial

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
@@ -25,11 +25,11 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   )
 }
 
-AboutPageTemplate.propTypes = {
-  title: PropTypes.string.isRequired,
-  content: PropTypes.string,
-  contentComponent: PropTypes.func,
-}
+// AboutPageTemplate.propTypes = {
+//   title: PropTypes.string.isRequired,
+//   content: PropTypes.string,
+//   contentComponent: PropTypes.func,
+// }
 
 const AboutPage = ({ data }) => {
   const { markdownRemark: post } = data
@@ -45,9 +45,9 @@ const AboutPage = ({ data }) => {
   )
 }
 
-AboutPage.propTypes = {
-  data: PropTypes.object.isRequired,
-}
+// AboutPage.propTypes = {
+//   data: PropTypes.object.isRequired,
+// }
 
 export default AboutPage
 
